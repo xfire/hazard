0c74e6b095a0b4956589e944b2df5c05978442e2
Rico Schiekel <fire@downgra.de>
2011-08-21 16:17:32 +0200

    io bound tests are now run in a uniq temporary directory

--- test/suite/Hazard/Tests/Core.hs
+++ test/suite/Hazard/Tests/Core.hs
@@ -15,24 +15,22 @@ tests = [ normalContentIsCopied
 
 normalContentIsCopied :: Test
 normalContentIsCopied = testCase
-    "normal files and directories are copied from the input to the output directory" $ do
-        let outdir = "output"
-        initOutputDirectory outdir
-        hazard $ HConfig { inputDir = "data/simple"
-                         , outputDir = outdir
-                         }
-        assertDirectoryContents "some of the normal files or directories are not copied to the output directory"
-                                outdir
-                                ["foo.txt", "bar.html", "spam.jpg", "eggs", "someDir", "someDir/someFile"]
+    "normal files and directories are copied from the input to the output directory" $
+        withSystemTempDirectory "hazardTests." $ \outdir -> do
+            hazard $ HConfig { inputDir = "data/simple"
+                             , outputDir = outdir
+                             }
+            assertDirectoryContents "some of the normal files or directories are not copied to the output directory"
+                                    outdir
+                                    ["foo.txt", "bar.html", "spam.jpg", "eggs", "someDir", "someDir/someFile"]
 
 specialContentIsNotCopied :: Test
 specialContentIsNotCopied = testCase
-    "special files and directories beginning with an underscore are not copied to the output directory" $ do
-        let outdir = "output"
-        initOutputDirectory outdir
-        hazard $ HConfig { inputDir = "data/mixed"
-                         , outputDir = outdir
-                         }
-        assertDirectoryContents "some of the special files are not ignored by the copy process"
-                                outdir
-                                ["foo.txt", "bar.html", "someDir", "someDir/someFile"]
+    "special files and directories beginning with an underscore are not copied to the output directory" $
+        withSystemTempDirectory "hazardTests." $ \outdir -> do
+            hazard $ HConfig { inputDir = "data/mixed"
+                             , outputDir = outdir
+                             }
+            assertDirectoryContents "some of the special files are not ignored by the copy process"
+                                    outdir
+                                    ["foo.txt", "bar.html", "someDir", "someDir/someFile"]
--- test/suite/Hazard/Tests/TestUtils.hs
+++ test/suite/Hazard/Tests/TestUtils.hs
@@ -1,22 +1,20 @@
 module Hazard.Tests.TestUtils
-    ( initOutputDirectory
-    , assertDirectoryContents
+    ( assertDirectoryContents
+    , withTempDirectory
+    , withSystemTempDirectory
     ) where
 
-import Control.Monad (when, liftM)
+import Control.Monad (liftM)
 import Control.Applicative ((<$>))
 import Data.List (sort)
-import System.Directory (doesDirectoryExist, removeDirectoryRecursive, createDirectory, getDirectoryContents)
+import System.Directory (doesDirectoryExist, removeDirectoryRecursive, createDirectory, getDirectoryContents, getTemporaryDirectory)
 import System.FilePath((</>), joinPath, splitPath)
 import Test.HUnit hiding (Test, path)
 
--- |Initialize the output directory. If necessary remove it and then create a new one.
-initOutputDirectory :: FilePath   -- ^ Directory
-                    -> IO ()
-initOutputDirectory dir = do
-    isDir <- doesDirectoryExist dir
-    when (isDir) $ removeDirectoryRecursive dir
-    createDirectory dir
+import System.Posix.Types (CPid)
+import System.Posix.Process (getProcessID)
+import Control.Exception (bracket)
+import System.IO.Error (try, isAlreadyExistsError)
 
 
 -- |Assert whether a directory contains exactly the given filenames.
@@ -30,20 +28,23 @@ assertDirectoryContents prefix dir expected = do
     where cleanup = sort . filter (`notElem` [".", ".."])
 
 
--- |Get the contents of a directory recursive. e.g. flattening the directory structure.
+-- |Get the contents of a directory recursive by flattening the directory structure.
 getDirectoryContentsRecursive :: FilePath        -- ^ Directory
                               -> IO [FilePath]
 getDirectoryContentsRecursive p = liftM (addSpecialDirs . removeHead) (go p)
     where addSpecialDirs = (sd ++)
-          removeHead = map (joinPath . tail . splitPath)
+          removeHead = map (joinPath . (drop basePathSize) . splitPath)
           go path = do contents <- filter (`notElem` sd) <$> getDirectoryContents path
                        (files, dirs) <- partitionM ((liftM not) . doesDirectoryExist) $ map (path </>) contents
                        recDirs <- mapM go dirs
                        return $ files ++ dirs ++ (concat recDirs)
           sd = [".", ".."]
+          basePathSize = length . splitPath $ p
 
 
 -- |Monadic partition function.
+--
+-- Behaves exactly the same as 'Predef.partition', except it works in a monadic context.
 partitionM :: (Monad m) => (a -> m Bool) -> [a] -> m ([a], [a])
 partitionM _ [] = return ([], [])
 partitionM p (x:xs) = do
@@ -51,3 +52,52 @@ partitionM p (x:xs) = do
     r <- p x
     if r then return (x:t,   f)
          else return (  t, x:f)
+
+-- | Create and use a temporary directory in the system standard temporary directory.
+--
+-- Behaves exactly the same as 'withTempDirectory', except that the parent temporary directory
+-- will be that returned by 'getTemporaryDirectory'.
+withSystemTempDirectory :: String             -- ^ Directory name prefix
+                        -> (FilePath -> IO a) -- ^ Callback that can use the directory
+                        -> IO a
+withSystemTempDirectory prefix action = do
+    tmpDir <- getTemporaryDirectory 
+    withTempDirectory tmpDir prefix action
+
+
+-- | Create and use a temporary directory.
+--
+-- Creates a new temporary directory inside the given directory, making use
+-- of the prefix. The temp directory is deleted after use. For example:
+--
+-- > withTempDirectory "src" "foo" $ \tmpDir -> do ...
+withTempDirectory :: FilePath           -- ^ Directory to create the directory in
+                  -> String             -- ^ Directory name prefix
+                  -> (FilePath -> IO a) -- ^ Callback that can use the directory
+                  -> IO a
+withTempDirectory targetDir template =
+  bracket
+    (createTempDirectory targetDir template)
+    (removeDirectoryRecursive)
+
+-- | Try to create a new temporary directory.
+--
+-- Try to create a new temporary directory inside the given directory. The 
+-- current process id and a random number is added to the new directory name.
+-- 
+-- If the new directory already exists, the creation is retried with a new
+-- random number.
+createTempDirectory :: FilePath    -- ^ Directory to create the directory in
+                    -> String      -- ^ Directory name prefix
+                    -> IO FilePath -- ^ Name of the newly created temporary directory
+createTempDirectory base template = getProcessID >>= findTempName 0
+  where findTempName :: Int -> CPid -> IO FilePath
+        findTempName x pid = do
+          let dirpath = base </> template ++ show pid ++ show x
+          r <- try $ createDirectory dirpath
+          case r of
+            Right _ -> return dirpath
+            Left  e | isAlreadyExistsError e -> findTempName (x + 1) pid
+                    | otherwise              -> ioError e
+
+
